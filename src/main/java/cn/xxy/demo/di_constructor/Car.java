f8c558f38dd690067a6f122d3b9bac947f00d75b
XiaotonyFly <614186309@qq.com>
2019-03-15 17:36:01 +0800

    DI——Constructor

---
+++ src/main/java/cn/xxy/demo/di_constructor/Car.java
@@ -0,0 +1,12 @@
+package cn.xxy.demo.di_constructor;
+
+/**
+ * Created by xuxiaoyu on 2019/3/15.
+ */
+public class Car {
+
+    public Car(Double price) {
+        System.out.println(price);
+    }
+
+}
